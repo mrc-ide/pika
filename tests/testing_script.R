f44f0a8765012176a230632d8b99e46be09f120e
kylieainslie <ainslie.kylie@gmail.com>
2020-04-21 09:12:40 +0100

    create testing_script

---
+++ tests/testing_script.R
@@ -0,0 +1,6 @@
+# Testing Script ------------------------------------------------------------------------
+library(dplyr)
+# Read in data
+load("data/china_rt_estimates.rda")
+load("data/exante_movement_data.rda")
+
