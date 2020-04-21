8f00ea989c47dba79e3e0a1abea75bbb47893103
kylieainslie <ainslie.kylie@gmail.com>
2020-04-21 11:24:16 +0100

    update analysis functions:  - check for NAs in cross_corr  - remove create_lag_date()  - debug n argument in rolling_corr() - still buggy

--- R/analysis_functions.R
+++ R/analysis_functions.R
@@ -3,19 +3,35 @@
 
 #' This function determines lag at which the cross correlation is highest
 #' @param dat data frame with columns that correspond to two time series and grouping variable(s)
+#' @param date_var character string of the column name that corresponds to the date variable
+#' date_var must be specified if subset_date is not null.
 #' @param grp_var character string of column names in dat to be used as grouping variable(s)
 #' @param x_var primary time series (should be a column in dat)
 #' @param y_var secondary time series (should be a column in dat)
 #' @param max_lag integer value of the number of lags to perform cross correlation for
+#' @param subset_date a character string of the same format as the date variable
 #' @return tibble of lags by grp_var
 #' @keywords pika
 #' @export
 # Determine cross correlation and max lag between time series ---------------------------
-cross_corr <- function(dat, grp_var, x_var, y_var, max_lag = 20){
+cross_corr <- function(dat, date_var = NULL, grp_var, x_var, y_var, max_lag = 20,
+                       subset_date = NULL){
   # check for missing values ------------------------------------------------------------
-  if(is.na(dat[,c(x_var,y_var)])){warning("There are NAs in your data set and they
-                                          will be removed before calculating the
-                                          cross correlation")}
+  if(any(is.na(dat[,x_var])) || any(is.na(dat[,y_var]))){
+    warning("There are NAs in your data set and they will be removed before calculating
+            the cross correlation")
+  }
+
+  # check that date_var specified when subset_date is not null --------------------------
+  if(!is.null(subset_date) & is.null(date_var)){
+    stop("date_var must be specified when subset_date is non-null")
+  }
+
+  # subset data by subset_date if subset_date is not NULL -------------------------------
+  if (!is.null(subset_date)){
+    dat <- dat[ dat[, date_var] <= subset_date, ]
+   }
+
   # calculate cross correlation for different lags --------------------------------------
   rhos <- dat %>%
     rename(x_var = {{x_var}}, y_var = {{y_var}}) %>% # rename x_var and y_var for ccf ---
@@ -24,9 +40,10 @@ cross_corr <- function(dat, grp_var, x_var, y_var, max_lag = 20){
     mutate_at("gg",purrr::map, function(x) ccf(x$x_var, x$y_var, lag.max = max_lag))
 
   # determine lags with max cross correlation -------------------------------------------
-  lags_max <- numeric(nrow(rhos)) # empty vector for max lag values by grp_var
-  # loop through grp_var values to determine max lag for each member of grp_var ---------
-  for (p in 1:nrow(rhos)){
+  lags_max <- numeric(nrow(rhos)) # empty vector for max lag values by grp_var ----------
+
+  for (p in 1:nrow(rhos)){ # loop through grp_var values to determine max lag for each --
+                           # member of grp_var ------------------------------------------
     df <- tibble(
       name = rhos[p,1],
       lags = rhos$gg[[p]]$lag[,1,1],
@@ -46,49 +63,47 @@ cross_corr <- function(dat, grp_var, x_var, y_var, max_lag = 20){
 }
 
 
-#' This function splits the time series to lag the primary without impacting the date of the secondary
-#' @param dat_x data frame with at least three columns: date, x_var, grp_var
-#' @param dat_y data frame with at least three columns: date, y_var, grp_var
-#' @param date_var date variable name in dat_x and dat_y
-#' @param x_var primary time series (should be a column in dat_x)
-#' @param y_var secondary time series (should be a column in dat_y)
-#' @param grp_var character string of column names in dat to be used as grouping variable(s)
-#' @param my_lag integer value of the lag
-#' @return data frame
-#' @keywords pika
-#' @export
-create_lag_date <- function(dat_x, dat_y, date_var, x_var, y_var, grp_var, my_lag){
-# lag the date variable -----------------------------------------------------------------
-dat1 <- dat_x %>%
-  mutate({{date_end}} = {{date_end}} + my_lag)
-
-dat1 %>%
-  left_join(., dat_y, by = c(date_var, grp_var))
-}
-
-
 #' This function calculates the rolling correlation between two time series
 #' @param dat data frame with columns that correspond to two time series and grouping variable(s)
 #' @param grp_var character string of column names in dat to be used as grouping variable(s)
 #' @param x_var primary time series (should be a column in dat)
 #' @param y_var secondary time series (should be a column in dat)
-#' @param n the number of time points to calculate the rolling correlation for
+#' @param period the time period over which to calculate rolling corr, values = c("weekly", "biweekly")
 #' @return tibble of lags by grp_var
 #' @keywords pika
 #' @export
-# Determine rolling correlation between R_t and movement ---------------------------
-rolling_corr <- function(dat, grp_var, x_var, y_var, n = 7){
-  dat %>%
-    group_by({{ grp_var }}) %>%
-    # group_by(!! enquo(grp_var)) %>%
-    mutate(x = {{ x_var }}, y = {{ y_var }}) %>%
-    tq_transmute_xy(
-      x = x,
-      y = y,
-      mutate_fun = runCor,
-      n = n,
+# Determine rolling correlation between two time series ----------------------------------
+rolling_corr <- function(dat, grp_var, x_var, y_var, period = "weekly"){
+  # a little bit of data wrangling to feed into tq_transmutate_xy ------------------------
+  dat1 <- dat %>%
+    rename(x = {{x_var}}, y = {{y_var}}, grp = {{ grp_var }}) %>%
+    filter(!is.na(x), !is.na(y)) %>%
+    group_by(grp)
+
+  # if period == "weekly", n = 7 ---------------------------------------------------------
+  if(period == "weekly"){
+    dat1 %>%
+      tq_transmute_xy(
+        x = x,
+        y = y,
+        mutate_fun = runCor,
+        n = 7,
       col_rename = "roll_corr"
     )
+  }
+
+  # if period == "biweekly", n = 14 ------------------------------------------------------
+  if(period == "biweekly"){
+    dat1 %>%
+      tq_transmute_xy(
+        x = x,
+        y = y,
+        mutate_fun = runCor,
+        n = 14,
+        col_rename = "roll_corr"
+      )
+  }
+
 }
 
 
