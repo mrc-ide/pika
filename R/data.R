2465f5b6e679eb202422a946528ea0e2f538c837
kylieainslie <ainslie.kylie@gmail.com>
2020-04-27 13:39:19 +0100

    document exante_movement_data

--- R/data.R
+++ R/data.R
@@ -29,6 +29,17 @@
 #' }
 "china_rt_estimates"
 
-#' Example exante movement data
-#' @format A data frame
+#' Daily within-city movement data for different regions in China
+#'
+#' The daily within-city movement data, used as a proxy for economic activity, is available from 1 January to
+#' 24 March 2020 for major metropolitan cities within each province in mainland China, Hong Kong SAR, and Macau SAR.
+#' These data, provided by Exante Data Inc, measured travel activity relative to the 2019 average (excluding Lunar
+#' New Year). The underlying data are based on near real-time people movement statistics from Baidu.
+#'
+#' @format A data frame with 672 rows and 3 variables:
+#'  \describe{
+#'    \item{date}{date, in YYYY-MM-DD format}
+#'    \item{province}{name of province/region in China where cases occured}
+#'    \item{movement}{daily population-weighted with-in city movement index}
+#' }
 "exante_movement_data"
