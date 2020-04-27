fbc02659f7ee9f421c891656d523dbf7073c910e
kylieainslie <ainslie.kylie@gmail.com>
2020-04-27 13:35:07 +0100

    document china_rt_estimates

--- R/data.R
+++ R/data.R
@@ -7,7 +7,7 @@
 #' links and/or clinical symptoms, and are detected with SARS-CoV-2 by PCR tests. However, in Hubei province,
 #' clinically diagnosed cases were additionally included between 12 and 19 February.
 #'
-#' @format A data frame with XX rows and 3 variables:
+#' @format A data frame with 483 rows and 3 variables:
 #' \describe{
 #'    \item{date}{date, in YYYY-MM-DD format}
 #'    \item{province}{name of province/region in China where cases occured}
@@ -16,8 +16,17 @@
 #' @source \url{http://2019ncov.chinacdc.cn/2019-nCoV/}
 "china_case_data"
 
-#' Example chinese rt data
-#' @format A data frame
+#' Daily estimates of effective reporduction number from COVID-19 case data in China
+#'
+#' A dataset of effective reproduction number (Rt) estimates over time in different provinces of China.
+#' @format A data frame with 496 rows and 5 variables:
+#'  \describe{
+#'    \item{date}{date, in YYYY-MM-DD format}
+#'    \item{province}{name of province/region in China where cases occured}
+#'    \item{r_mean}{mean effective reproduction number}
+#'    \item{r_q2.5}{lower bound of the 95% credible interval for mean effective reproduction number}
+#'    \item{r_q97.5}{upper bound of the 95% credible interval for mean effective reproduction number}
+#' }
 "china_rt_estimates"
 
 #' Example exante movement data
