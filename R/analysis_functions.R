b6dacf5cdee88922435783f33a7be99cc33fa90f
kylieainslie <ainslie.kylie@gmail.com>
2020-05-12 11:33:51 +0100

    fix error start_date check

--- R/analysis_functions.R
+++ R/analysis_functions.R
@@ -192,8 +192,10 @@ calc_percent_change <- function(dat, date_var = "date", grp_var, count_var,
   }
 
   # chack that start_date is a valid format ----------------------------------------------
-  if(!is.null(start_date) & (start_date %in% dat[,date_var]) == FALSE){
-    stop("start_date does not match any dates in input dataset")
+  if(!is.null(start_date)){
+    if((start_date %in% dat[,date_var]) == FALSE){
+      stop("start_date does not match any dates in input dataset")
+    }
   }
 
   dat1 <- dat %>%
